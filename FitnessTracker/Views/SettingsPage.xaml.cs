4d76db91116157c39706fa263d91df4d92bcb57a
AravinthSundaramSF <aravinth.sundaram@syncfusion.com>
2025-02-11 15:41:25 +0530

    937968: Implement the settings page for the fitness tracker application in Maui

---
+++ FitnessTracker/Views/SettingsPage.xaml.cs
@@ -0,0 +1,43 @@
+namespace FitnessTracker.Views;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+
+    private void ProfilePhoto_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+    private void Profile_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void Notification_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void Appearance_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void Account_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void Help_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void Logout_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+}
