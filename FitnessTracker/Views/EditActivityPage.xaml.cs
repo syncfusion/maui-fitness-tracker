42de088d47a8e768b7bafc3fc5bdb3c213f7d069
ShyamSundar-SF4699 <shyamsundar.d@syncfusion.com>
2025-02-17 14:58:41 +0530

    Updated EditActivityPage with date and time picker.

--- FitnessTracker/Views/EditActivityPage.xaml.cs
+++ FitnessTracker/Views/EditActivityPage.xaml.cs
@@ -1,32 +1,70 @@
 ﻿
+using Syncfusion.Maui.Picker;
+
 namespace FitnessTracker.Views
 {
 	public partial class EditActivityPage : ContentPage
 	{
         List<string> activityList = new List<string> { "Walking", "Running", "Cycling", "Swimming", "Hiking", "Aerobics", "Elliptical Training", "Strength Training", "Stair Climbing", "Yoga", "Dancing", "Martial Arts", "Pilates", "Meditation", "Rowing", "CrossFit" };
-        List<string> timeSlots = new List<string>();
         public EditActivityPage ()
 		{
 			InitializeComponent ();
-            LoadStartEndTime ();
             activityBox.ItemsSource = activityList;
             activityBox.SelectedIndex = 0;
 		}
 
-        void LoadStartEndTime()
+        private void datePickerEntry_Focused(object sender, FocusEventArgs e)
         {
-            for (int hour = 0; hour < 24; hour++)
-            {
-                for (int minute = 0; minute < 60; minute += 15)
-                {
-                    string period = hour < 12 ? "am" : "pm";
-                    int displayHour = (hour % 12 == 0) ? 12 : hour % 12;
-                    timeSlots.Add($"{displayHour}:{minute:00} {period}");
-                }
-            }
+            datePicker.IsOpen = true;
+        }
 
-            startTimeBox.ItemsSource = timeSlots;
-            endTimeBox.ItemsSource = timeSlots;
+        private void DatePicker_Tapped(object sender, TappedEventArgs e)
+        {
+            datePicker.IsOpen = true;
+        }
+
+        private void datePicker_SelectionChanged(object sender, Syncfusion.Maui.Picker.DatePickerSelectionChangedEventArgs e)
+        {
+            if (datePicker.SelectedDate != null)
+            {
+                datePickerEntry.Text = datePicker.SelectedDate.Value.ToString("MM/dd/yyyy");
+            }
+        }
+
+        private void startTimePickerEntry_Focused(object sender, FocusEventArgs e)
+        {
+            startTimePicker.IsOpen = true;
+        }
+
+        private void StartTimePicker_Tapped(object sender, TappedEventArgs e)
+        {
+            startTimePicker.IsOpen = true;
+        }
+
+        private void startTimePicker_SelectionChanged(object sender, TimePickerSelectionChangedEventArgs e)
+        {
+            if (startTimePicker.SelectedTime != null)
+            {
+                startTimePickerEntry.Text = DateTime.Today.Add(startTimePicker.SelectedTime.Value).ToString("hh:mm tt");
+            }
+        }
+
+        private void endTimePickerEntry_Focused(object sender, FocusEventArgs e)
+        {
+            endTimePicker.IsOpen = true;
+        }
+
+        private void EndTimePicker_Tapped(object sender, TappedEventArgs e)
+        {
+            endTimePicker.IsOpen = true;
+        }
+
+        private void endTimePicker_SelectionChanged(object sender, TimePickerSelectionChangedEventArgs e)
+        {
+            if (endTimePicker.SelectedTime != null)
+            {
+                endTimePickerEntry.Text = DateTime.Today.Add(endTimePicker.SelectedTime.Value).ToString("hh:mm tt");
+            }
         }
     }
 }
