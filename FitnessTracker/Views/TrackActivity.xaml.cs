ec5d1e5a6ddce736d78bbe88bdf9474bbfd4293e
ShyamSundar-SF4699 <shyamsundar.d@syncfusion.com>
2025-03-11 15:45:43 +0530

    Updated theme in AddActivityPage.xaml and TrackActivity.xaml

--- FitnessTracker/Views/TrackActivity.xaml.cs
+++ FitnessTracker/Views/TrackActivity.xaml.cs
@@ -5,7 +5,7 @@ namespace FitnessTracker.Views
 {
     public partial class TrackActivity : ContentPage
     {
-        List<string> activityList = new List<string>{ "Walking", "Running", "Cycling", "Swimming", "Hiking", "Aerobics", "Elliptical Training", "Strength Training", "Stair Climbing", "Yoga", "Dancing", "Martial Arts", "Pilates", "Meditation", "Rowing", "CrossFit" };
+        List<string> activityList = new List<string>{ "Walking", "Running", "Cycling", "Swimming", "Yoga", "Sleeping" };
         public TrackActivity()
         {
             InitializeComponent();
