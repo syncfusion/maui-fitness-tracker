edda6eb920b1234c5a14a39436201a0b52edef79
ShyamSundar-SF4699 <shyamsundar.d@syncfusion.com>
2025-08-29 10:02:46 +0530

    Committed the missed file

---
+++ FitnessTracker/Converters/TooltipValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace FitnessTracker
+{
+    public class TooltipValueConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is DataPoint model)
+            {
+                switch (parameter?.ToString())
+                {
+                    case "Name":
+                        return model.Label;
+                    case "Value":
+                        return model.Value;
+                }
+            }
+            return value;
+        }
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
